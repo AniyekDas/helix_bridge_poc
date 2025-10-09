12624921dbbcda09c2061eb7662ae34e7eb48ec5
AniyekDas <aniyek.das@pfizer.com>
2025-10-09 22:02:23 +0530

    Updated Blocks Function

--- import_os_func.py
+++ import_os_func.py
@@ -1,9 +1,109 @@
 import os
 import json
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError
 from dotenv import load_dotenv
 import csv
 import time
+
+def create_block(page, b_title, b_description, b_category, b_protected, b_files, b_auto_attach, b_auto_attach_location, b_auto_attach_exceptions, b_auto_attach_to_error_pages, b_html, b_css):
+    # Fill title
+    b_title_field = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[1]/input')
+    b_title_field.click()
+    page.keyboard.press("Control+A")
+    b_title_field.fill(b_title)
+    page.wait_for_timeout(1000)
+
+    # Fill Description
+    b_description_field = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[2]/input')
+    b_description_field.click()
+    page.keyboard.press("Control+A")
+    b_description_field.fill(b_description)
+    page.wait_for_timeout(1000)
+
+    # Fill category
+    page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[3]/div/div[2]').click()
+    b_category_field = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[3]/div/div[2]/input')
+    b_category_field.fill(b_category)
+    page.keyboard.press("Enter")
+    page.wait_for_timeout(1000)
+
+    # If protected, perform extended logic
+    if b_protected:
+        page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div/label/input').click()
+        page.wait_for_timeout(1000)
+
+        if isinstance(b_files, list) and b_files:
+            for file_value in b_files:
+                page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/section/section/div/div[3]').click()
+                file_input = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/section/section/div/div[3]/input')
+                file_input.fill(file_value)
+                page.keyboard.press("Enter")
+                page.wait_for_timeout(1000)
+
+        if b_auto_attach:
+            page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[2]/label/input').click()
+            page.wait_for_timeout(1000)
+
+            if b_auto_attach_location:
+                location_select = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[3]/select')
+                options = location_select.locator('option').all()
+                for option in options:
+                    option_text = option.text_content()
+                    if option_text.strip() == b_auto_attach_location.strip():
+                        option.click()
+                        page.wait_for_timeout(1000)
+                        break
+
+            if isinstance(b_auto_attach_exceptions, list) and b_auto_attach_exceptions:
+                exception_container = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]/div/div[2]')
+                page.wait_for_timeout(1000)
+                for exception in b_auto_attach_exceptions:
+                    exception_container.click()
+                    exception_input = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]/div/div[2]/input')
+                    exception_input.fill(exception)
+                    page.keyboard.press("Enter")
+                    page.wait_for_timeout(1000)
+
+        if b_auto_attach_to_error_pages:
+            page.locator('xpath=b_auto_attach_to_error_pages').click()
+            page.wait_for_timeout(1000)
+
+    # Proceed with block import
+    import_btn = page.locator('.fa-download')
+    import_btn.click() # click import button
+    page.wait_for_timeout(1000)
+    text_field = page.locator('xpath=//*[@id="gjs-mdl-c"]/div/div/div[6]/div[1]/div/div/div/div[5]/div/pre')
+    text_field.click() #click the text area
+    page.wait_for_timeout(1000)
+    text_fill = page.locator('xpath=//*[@id="gjs-mdl-c"]/div/div/div[1]/textarea')
+    text_fill.fill(b_html + "\n" + "<style>\n" + b_css + "\n</style>") # fill the text area
+    page.wait_for_timeout(2000)
+    page.locator('.gjs-btn-import').click() # click import save button
+    page.wait_for_timeout(1000)
+    page.locator('xpath=//*[@id="wrapper"]/nav/div[2]/div[1]/div[2]/div/div/button[1]').click() # click the save button
+    page.wait_for_timeout(2000)
+    page.locator('.btn-back-tiered-menu ').click() # click back button
+    page.wait_for_timeout(2000)
+
+
+def search_and_check_block_existence(page, b_title):
+    search_box = page.locator('xpath=//*[@id="search-input"]')
+    search_box.click()
+    page.keyboard.press("Control+A")
+    search_box.fill(b_title)
+    search_box.press("Enter")
+    page.wait_for_timeout(2000)
+
+    block_exists = False
+    if page.locator(f"text={b_title}").is_visible():
+        rows = page.query_selector_all('table[data-v-4aee22f3][data-v-816643a6] tbody tr')
+        for i, row in enumerate(rows):
+            second_column = row.query_selector("td:nth-child(2)")
+            if second_column.inner_text().strip() == b_title:
+                block_exists = True
+                print(f"'{b_title} already exists")
+    return block_exists
+
 
 def process_blocks(page, sitename, instance_id, blocks_folder):
     page.goto(f"https://{sitename}/builder/website/{instance_id}?panel=left-sidebar-settings--elements")
@@ -18,125 +118,43 @@ def process_blocks(page, sitename, instance_id, blocks_folder):
                     b_css = block_data.get("storage", {}).get("data", {}).get("css", "")
                     b_html = block_data.get("storage", {}).get("data", {}).get("html", "")
                     b_title = block_data.get("settings", {}).get("title", "")
-                    b_description = block_data.get("settings", {}).get("settings", {}).get("description", "")
+                    settings_settings = block_data.get("settings", {}).get("settings") or {}
+                    b_description = settings_settings.get("description", "")
+                    b_deleted_by = settings_settings.get("deleted_by", "")
                     b_category = block_data.get("settings", {}).get("category", "")
                     b_protected = block_data.get("settings", {}).get("protected", "")
                     b_files = block_data.get("settings", {}).get("files", [])
-                    b_auto_attach = block_data.get("settings", {}).get("settings", {}).get("auto_attach", False)
-                    b_auto_attach_location = block_data.get("settings", {}).get("settings", {}).get("auto_attach_location", "")
-                    b_auto_attach_exceptions = block_data.get("settings", {}).get("settings", {}).get("auto_attach_exceptions", [])
-                    b_auto_attach_to_error_pages = block_data.get("settings", {}).get("settings", {}).get("auto_attach_to_error_pages", False)
+                    b_auto_attach = settings_settings.get("auto_attach", False)
+                    b_auto_attach_location = settings_settings.get("auto_attach_location", "")
+                    b_auto_attach_exceptions = settings_settings.get("auto_attach_exceptions", [])
+                    b_auto_attach_to_error_pages = settings_settings.get("auto_attach_to_error_pages", False)
 
-                    # if isinstance(b_files, list) and b_files:
-                    #     block_title_map[block_name.replace(".json", "")] = b_title
+                    if b_deleted_by:
+                        print(f"Skipping block '{b_title}' as it was deleted by {b_deleted_by}. Exiting block processing.")
+                    
+                    block_exists = search_and_check_block_existence(page, b_title)
 
                     print(f"found block : {block_name}")
 
-                    # click on add block
-                    # page.locator('xpath=//*[@id="webbuilder-modal-block-list"]/div/div/div/div[1]/div[2]/a').click()
-                    # page.wait_for_timeout(1000)
+                    add_block_button = page.locator('xpath=//*[@id="webbuilder-modal-block-list"]/div/div/div/div[1]/div[2]/a')
+                    fresh_site_button = page.locator('xpath=//*[@id="webbuilder-modal-block-list"]/div/div/div/a')
 
-
-                    primary_button = page.locator('xpath=//*[@id="webbuilder-modal-block-list"]/div/div/div/div[1]/div[2]/a')
-                    fallback_button = page.locator('xpath=//*[@id="webbuilder-modal-block-list"]/div/div/div/a')
-
-                    if primary_button.is_visible():
-                        primary_button.click()
-                        print("Clicked primary block list button.")
-                    elif fallback_button.is_visible():
-                        fallback_button.click()
-                        print("Clicked fallback block list button.")
+                    if add_block_button.is_visible() and not block_exists and not b_deleted_by:
+                        add_block_button.click()
+                        print("Clicked Add block list button.")
+                        page.wait_for_timeout(1000)
+                        create_block(page, b_title, b_description, b_category, b_protected, b_files, b_auto_attach, b_auto_attach_location, b_auto_attach_exceptions, b_auto_attach_to_error_pages, b_html, b_css)
+                    elif fresh_site_button.is_visible() and not b_deleted_by:
+                        fresh_site_button.click()
+                        print("Clicked New site block list button.")
+                        page.wait_for_timeout(1000)
+                        create_block(page, b_title, b_description, b_category, b_protected, b_files, b_auto_attach, b_auto_attach_location, b_auto_attach_exceptions, b_auto_attach_to_error_pages, b_html, b_css)
                     else:
                         print("Neither block list button was found.")
 
-
-                    # Fill title
-                    b_title_field = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[1]/input')
-                    b_title_field.click()
-                    page.keyboard.press("Control+A")
-                    b_title_field.fill(b_title)
-                    page.wait_for_timeout(1000)
-
-                    # Fill Description
-                    b_description_field = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[2]/input')
-                    b_description_field.click()
-                    page.keyboard.press("Control+A")
-                    b_description_field.fill(b_description)
-                    page.wait_for_timeout(1000)
-
-                    # Fill category
-                    page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[3]/div/div[2]').click()
-                    b_category_field = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[1]/div[3]/div/div[2]/input')
-                    b_category_field.fill(b_category)
-                    page.keyboard.press("Enter")
-                    page.wait_for_timeout(1000)
-
-                    # If protected, perform extended logic
-                    if b_protected:
-                        # Click protected checkbox
-                        page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div/label/input').click()
-                        page.wait_for_timeout(1000)
-
-                        # Step 1: Handle b_files
-                        if isinstance(b_files, list) and b_files:
-                            for file_value in b_files:
-                                page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/section/section/div/div[3]').click()
-                                file_input = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/section/section/div/div[3]/input')
-                                file_input.fill(file_value)
-                                page.keyboard.press("Enter")
-                                page.wait_for_timeout(1000)
-
-                        # Step 2: Handle auto_attach
-                        if b_auto_attach:
-                            page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[2]/label/input').click()
-                            page.wait_for_timeout(1000)
-
-                            # Step 2a: Handle auto_attach_location
-                            if b_auto_attach_location:
-                                location_select = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[3]/select')
-                                options = location_select.locator('option').all()
-                                for option in options:
-                                    option_text = option.text_content()
-                                    if option_text.strip() == b_auto_attach_location.strip():
-                                        option.click()
-                                        page.wait_for_timeout(1000)
-                                        break
-
-                            # Step 3: Handle auto_attach_exceptions
-                            if isinstance(b_auto_attach_exceptions, list) and b_auto_attach_exceptions:
-                                exception_container = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]/div/div[2]')
-                                page.wait_for_timeout(1000)
-                                for exception in b_auto_attach_exceptions:
-                                    exception_container.click()
-                                    exception_input = page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]/div/div[2]/input')
-                                    exception_input.fill(exception)
-                                    page.keyboard.press("Enter")
-                                    page.wait_for_timeout(1000)
-
-                        # Step 4: Handle auto_attach_to_error_pages
-                        if b_auto_attach_to_error_pages:
-                            page.locator('xpath=b_auto_attach_to_error_pages').click()
-                            page.wait_for_timeout(1000)
-
-                    # Proceed with block import
-                    import_btn = page.locator('.fa-download')
-                    import_btn.click()
-                    page.wait_for_timeout(1000)
-                    text_field = page.locator('xpath=//*[@id="gjs-mdl-c"]/div/div/div[6]/div[1]/div/div/div/div[5]/div/pre')
-                    text_field.click()
-                    page.wait_for_timeout(1000)
-                    text_fill = page.locator('xpath=//*[@id="gjs-mdl-c"]/div/div/div[1]/textarea')
-                    text_fill.fill(b_html + "\n" + "<style>\n" + b_css + "\n</style>")
-                    page.wait_for_timeout(2000)
-                    page.locator('.gjs-btn-import').click() # save code button (import)
-                    page.wait_for_timeout(1000)
-                    page.locator('xpath=//*[@id="wrapper"]/nav/div[2]/div[1]/div[2]/div/div/button[1]').click() # save button
-                    page.wait_for_timeout(2000)
-                    page.locator('.btn-back-tiered-menu ').click() # back button press
-                    page.wait_for_timeout(2000)
-
                 except KeyError:
                     print(f"Certain Field Not Found : '{block_name}'")
+
 
 def process_files(page, sitename, instance_id, files_folder, pages_folder):
     # Navigate to Content > Files
@@ -283,9 +301,10 @@ def process_files(page, sitename, instance_id, files_folder, pages_folder):
                                             page.wait_for_timeout(700)
                     # Paste file details in field
                     if f_private != False:
-                        private_field = page.locator('xpath=//*[@id="privateFile"]')
-                        private_field.click()
-                        page.wait_for_timeout(1000)
+                        private_field = page.locator('xpath=//*[@id="privateFile"]').visible()
+                        if private_field.visible():
+                            private_field.click()
+                            page.wait_for_timeout(1000)
 
                     # Only proceed if header or footer is set and the section is visible
                     if (f_header_str != "0" or f_footer_str != "0") and page.locator('xpath=//*[@id="webbuilder-editor-content-wrapper"]/div/div[1]/div/div/div[3]/div[2]/div/div[2]/div/div/div/div/div/form/div[2]/div[2]/div[3]').is_visible():
